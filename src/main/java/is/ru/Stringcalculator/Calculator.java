bf9555370ff6beb13455f59c8dad76b56a6c9152
baldvinj15 <balliholm88@gmail.com>
2016-10-12 17:04:24 +0000

    Test emtpy string

--- src/main/java/is/ru/Stringcalculator/Calculator.java
+++ src/main/java/is/ru/Stringcalculator/Calculator.java
@@ -1,16 +1,8 @@
 package is.ru.Stringcalculator;
 
-puglic class Calculator {
+public class Calculator {
 
 	public static int add(String text){
-		if(text.equals("")){
-			return 0;
-		}
-		else if(text.contains(",")){
-			return sum(SplitNumbers(text));
-		}
-		else
-			return 1;
+		return 0;
 	}
-
 }
