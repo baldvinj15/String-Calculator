da9081a43fe844c07bde036a47e207e118d55f00
baldvinj15 <balliholm88@gmail.com>
2016-10-13 22:08:08 +0000

    Refactored code

--- src/main/java/is/ru/Stringcalculator/Calculator.java
+++ src/main/java/is/ru/Stringcalculator/Calculator.java
@@ -5,12 +5,29 @@ public class Calculator {
 	public static int add(String text){
 		if(text.equals(""))
 			return 0;
+
 		else if(text.contains(",")) {
-			String [] num = text.split(",");
-			return Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+			return sum(splitNumbers(text));
 			}
+
 		else
 			return 1;
 	}
+
+	private static String[] splitNumbers(String numbers){
+	    return numbers.split(",");
+	}
+
+	static int toInt(String text) {
+		return Integer.parseInt(text);
+	}
+
+	private static int sum(String[] numbers){
+ 	    int total = 0;
+        for(String number : numbers){
+		    total += toInt(number);
+		}
+		return total;
+    }
 
 }
